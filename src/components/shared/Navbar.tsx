239124305c7e7f87271cd6814772333867113a2d
smsaifurrahman <smsaifur525@gmail.com>
2025-07-17 07:51:30 +0600

    Add Redux cart slice with product add, increment, decrement, and remove logic

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -35,10 +35,12 @@ export default function Navbar() {
    return (
       <header className="border-b bg-background w-full sticky top-0 z-10">
          <div className="container flex justify-between items-center mx-auto h-16 px-3">
-            <h1 className="text-2xl font-black flex items-center">
-               <Logo />
-               Next Mart
-            </h1>
+            <Link href={'/'}>
+               <h1 className="text-2xl font-black flex items-center">
+                  <Logo />
+                  Next Mart
+               </h1>
+            </Link>
             <div className="max-w-md  flex-grow">
                <input
                   type="text"
--- src/redux/features/cartSlice.ts
+++ src/redux/features/cartSlice.ts
@@ -1,13 +1,15 @@
 /** @format */
 
-import { RootState } from "./../../../node_modules/reselect/src/createSelectorCreator";
-/** @format */
-
 import { IProduct } from "@/types";
 import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+export interface CartProduct extends IProduct {
+   orderQuantity: number;
+}
 
 interface InitialState {
-   products: IProduct[];
+   products: CartProduct[];
 }
 
 const initialState: InitialState = {
@@ -19,7 +21,38 @@ const cartSlice = createSlice({
    initialState,
    reducers: {
       addProduct: (state, action) => {
-         state.products.push(action.payload);
+         const productToAdd = state.products.find(
+            (product) => product._id === action.payload._id
+         );
+
+         if (productToAdd) {
+            productToAdd.orderQuantity += 1;
+
+            return;
+         }
+
+         state.products.push({ ...action.payload, orderQuantity: 1 });
+      },
+      incrementOrderQuantity: (state, action) => {
+         const productToIncrement = state.products.find(
+            (product) => product._id === action.payload
+         );
+         if (productToIncrement) {
+            productToIncrement.orderQuantity += 1;
+            return;
+         }
+      },
+      decrementOrderQuantity: (state, action) => {
+         const productToIncrement = state.products.find(
+            (product) => product._id === action.payload
+         );
+         if (productToIncrement && productToIncrement.orderQuantity > 1) {
+            productToIncrement.orderQuantity -= 1;
+            return;
+         }
+      },
+      removeOrderQuantity: (state, action) => {
+         state.products = state.products.filter(product => product._id != action.payload ); 
       },
    },
 });
@@ -28,5 +61,5 @@ export const orderedProductSelector = (state: RootState) => {
    return state.cart.products;
 };
 
-export const { addProduct } = cartSlice.actions;
+export const { addProduct,incrementOrderQuantity, decrementOrderQuantity, removeOrderQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
